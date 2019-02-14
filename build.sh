e5b0585738a97d6e6f0f7b39153d867994a6de76
Adam Pritchard <pritchard.adam@gmail.com>
2019-02-14 12:49:53 -0500

    Move git state output to gradle task; add debug build

--- build.sh
+++ build.sh
@@ -4,4 +4,4 @@ set -xeuo pipefail
 git describe --always --long --dirty --tags > ./psicashlib/src/main/assets/git.txt
 
 ./gradlew :psicashlib:clean
-./gradlew :psicashlib:assembleRelease
+./gradlew :psicashlib:assembleDebug :psicashlib:assembleRelease
